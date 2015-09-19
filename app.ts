e1f932f90b571a9a6c5661f2141fe5ff7bf336fb
peterennis <peterennis@yahoo.com>
2015-09-18 18:44:31 -0700

    app outputs Alice Themes to check it still works with the prep additions for starting on ThemeSwitcher

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ import {Component, View, bootstrap} from 'angular2/angular2';
 class MyAppComponent {
   name: string;
   constructor() {
-    this.name = 'Alice';
+    this.name = 'Alice Themes';
   }
 }
 
